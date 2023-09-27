dc3d35ef9b1087d4e9a58f6178551f4f247a470e
deng.youxu <dyx1437418067@gmail.com>
2023-09-28 00:16:50 +0800

    20230928:宏注释...

--- jdk8u-dev/hotspot/src/share/vm/utilities/slog.cpp
+++ jdk8u-dev/hotspot/src/share/vm/utilities/slog.cpp
@@ -227,9 +227,9 @@ static size_t slog_get_kernel_tid()
      * pthread_self(): 用户态线程id
      * __NR_gettid: 内核线程id
      */
-#ifdef LINUX
+#if defined(LINUX)
     return (size_t)syscall(__NR_gettid);
-#elifdef __APPLE__
+#elif defined(__APPLE__)
     return (size_t)pthread_mach_thread_np(pthread_self());
 #endif
 }
